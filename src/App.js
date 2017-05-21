3c2dbbe4e2e71d0639815c3c134c72e8caddc5a6
Sarath <sarath.hacks@gmail.com>
2017-05-21 17:33:36 +0530

    React app booted

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Comingbanner from './components/Comingbanner';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Comingbanner/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Comingbanner.js
@@ -0,0 +1,13 @@
+import React , { Component } from 'react';
+import './Comingbanner.css'
+
+export default class Comingbanner extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1 className="the-logo"> vuelabs </h1>
+            </div>
+        )
+    }
+}
